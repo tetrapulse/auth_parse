2712ac56fae45f7e1e663d1024dc64f60c0d8ef7
tetrapulse <39704837+tetrapulse@users.noreply.github.com>
2019-05-11 22:07:11 -0600

    Update main.py
    Added exception handling for FileNotFound and Permission errors. Also added a few comments.

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import re
 import argparse
 
+# --- Begin argument parsing ---
 parser = argparse.ArgumentParser(description='Tool to analize ssh attempts '
 	'and show top offenders.')
+
 parser.add_argument('--log-file', '-l', default='/var/log/auth.log',
 	help='Authentication log file. Default is: /var/log/authlog')
+
 args = parser.parse_args()
+
 filename = args.log_file # argparse removes leading dashes and converts
 						 # internal dashes to underscores
+# --- End arguement parsing ---
 
 def parse_line(line):
 	# Searches provided line for failed logins and returns the 
@@ -40,22 +45,31 @@ def parse_line(line):
 # a database or dictionary. I will start with dictionarys
 failed_attemts = {}
 failed_username = {}
-with open(filename) as file_object:
-	for line in file_object:
-		result = parse_line(line)
-		# First assumes that an entry already exists for the current IP.
-		# Should that not be the case it will correct it by creating it new.
-		# Should both of those fail it will print a message letting the user know
-		if result:
-			try:
-				failed_attemts[result[1]] += 1
-			except:
-				failed_attemts[result[1]] = 1			
-			
-			try:
-				failed_username[result[1]].append(result[0])
-			except:
-				failed_username[result[1]] = [result[0]]
+try:
+	with open(filename) as file_object:
+		for line in file_object:
+			result = parse_line(line)
+			# First assumes that an entry already exists for the current IP.
+			# Should that not be the case it will correct it by creating it new.
+			# Should both of those fail it will print a message letting the user know
+			if result:
+				try:
+					failed_attemts[result[1]] += 1
+				except:
+					failed_attemts[result[1]] = 1			
+				
+				try:
+					failed_username[result[1]].append(result[0])
+				except:
+					failed_username[result[1]] = [result[0]]
+except FileNotFoundError as err:
+	print("The log file specified could not be found.")
+	print(err)
+	exit(2)
+except PermissionError as err:
+	print("You do not have permission to read this file, are you root?")
+	print(err)
+	exit(13)
 
 # If no items are added to the dictionarys the program will display a 
 # message and exit.
@@ -63,6 +77,8 @@ if not failed_attemts and not failed_username:
 	print(f'No ssh login attempts found in {filename}')
 	print('Exiting...')
 	exit(0)
+
+# Reporting to console begins here
 print("IP addresses:")
 for ip in failed_attemts.keys():
 	print("%s\t%s" % (ip, failed_attemts[ip]))		
